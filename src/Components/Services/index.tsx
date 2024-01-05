73b12fb26139948490b2d36b5a907858d66921ef
iamgerson <gersonpaulo114@gmail.com>
2024-01-05 11:39:38 +0100

    second commit

--- src/Components/Services/index.tsx
+++ src/Components/Services/index.tsx
@@ -6,14 +6,15 @@ export function ServicesSection() {
       <h3>Serviços</h3>
       <h2>Oferecemos os melhores Serviços.</h2>
       <GridContainer>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
+        <GridItem><h2>Programas de TV</h2></GridItem>
+        <GridItem><h2>Reportagem</h2></GridItem>
+        <GridItem><h2>Vídeo Clipe</h2></GridItem>
+        <GridItem><h2>Fotojornalismo</h2></GridItem>
+        <GridItem><h2>Fotografia</h2></GridItem>
+        <GridItem><h2>Tele-séries</h2></GridItem>
+        <GridItem><h2>Filmes</h2></GridItem>
+        <GridItem><h2>Cobertura de Eventos</h2></GridItem>
+        <GridItem><h2>Spots publicitários</h2></GridItem>
       </GridContainer>
     </SectionService>
   )
